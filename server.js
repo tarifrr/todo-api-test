75bbdcc91e4e7c7ab6f443a89e450d3692340bed
Tarif Rahman <tarifrr@brandeis.edu>
2017-01-26 18:40:38 -0500

    Yes

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var middleware = require('./middleware.js');
+const PORT = process.env.PORT || 3000;
+
+
+
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentiction);
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express!');
+// });
+
+app.get('/about', middleware.requireAuthentiction ,function(req, res){
+	res.send('About Us!!');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(PORT ,function(){
+	console.log('Server started at port : ' + PORT);
+});
