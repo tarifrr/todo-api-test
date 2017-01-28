6cfbc6d647e5376f32dba6c2a4f58407f021fe4f
Tarif Rahman <tarifrr@brandeis.edu>
2017-01-28 11:30:44 -0500

    Todo-Api updated server.js

--- server.js
+++ server.js
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.get('/',function(req,res){
 	res.send('Todo API Root');
 });
+
+app.listen(PORT, function(){
+	console.log('Express listening on port ' + PORT + ' !');
+});
