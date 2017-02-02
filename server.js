cdf3a22ccd56caac6f0dedfb5a09685a61351301
Tarif Rahman <tarifrr@brandeis.edu>
2017-02-02 00:45:33 -0500

    Added PUT request to TODO App

--- server.js
+++ server.js
@@ -68,6 +68,35 @@ app.delete('/todos/:id', function (req, res){
 
 });
 
+//PUT /todos/:id
+
+app.put('/todos/:id', function (req,res){
+	var body = _.pick(req.body,'description','completed');
+	var validAttributes = {};
+
+	var todoId = parseInt(req.params.id,10);
+	var matchedToDo = _.findWhere(todos,{id:todoId});
+
+	if(!matchedToDo)
+		return res.status(404).json({"error":"no todo found with provided id"});
+
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed; 
+	}else if (body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	}
+
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description.trim(); 
+	}else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}	
+
+	_.extend(matchedToDo,validAttributes);
+	res.json(matchedToDo);
+});
+
 
 app.listen(PORT, function(){
 	console.log('Express listening on port ' + PORT + ' !')
